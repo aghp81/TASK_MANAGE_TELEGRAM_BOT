c6544158c7b1572a39ebe1dd15cda4729a805638
aghp81 <aghp81@gmail.com>
2025-05-23 11:42:51 +0330

    مدل کاربر و تسک و فاز

--- database/models.py
+++ database/models.py
@@ -6,17 +6,31 @@ from datetime import datetime
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True, nullable=False, index=True)
+    username = Column(String)
+    full_name = Column(String)
+
+    tasks = relationship("Task", back_populates="user")
+
+
 class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     description = Column(String)
-    assigned_to = Column(String)  # می‌تونه آیدی تلگرام کاربر باشه
     deadline = Column(DateTime)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+    assigned_to_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user = relationship("User", back_populates="tasks")
+
     phases = relationship("Phase", back_populates="task", cascade="all, delete-orphan")
+
 
 class Phase(Base):
     __tablename__ = 'phases'
