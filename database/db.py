bbf2dcfd8fec30e041ff2d59be053060e84047db
aghp81 <aghp81@gmail.com>
2025-05-23 12:01:36 +0330

    from database.models import Base

--- database/db.py
+++ database/db.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from database.models import Base
 
 engine = create_engine("sqlite:///bot_database.db", echo=True)
 SessionLocal = sessionmaker(bind=engine)
