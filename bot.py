804305bf08b2654d5b7b4055f6e996f3b1851494
aghp81 <aghp81@gmail.com>
2025-05-23 10:39:29 +0330

    افزودن توکن و پراکسی برای اتصال به تلگرام

--- bot.py
+++ bot.py
@@ -1,24 +1,29 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties
 import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.filters import Command
+from aiogram.types import Message
 
-from config import BOT_TOKEN
-from database.base import init_db
-from handlers import start
+BOT_TOKEN = "7972234523:AAEsQ18N_JsdBrhfFZ8xK3MZ5kHjy_Ox4iI"
+proxy_url = "http://51.81.245.3:17981"  # آدرس پراکسی خودت رو اینجا بگذار
+
+async def start_handler(message: Message):
+    await message.answer("سلام! ربات مدیریت پروژه آماده است.")
 
 async def main():
-    bot = Bot(
-        token=BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-    )
+    # ساخت session با پراکسی
+    session = AiohttpSession(proxy=proxy_url)
+    bot = Bot(token=BOT_TOKEN, session=session)
     dp = Dispatcher()
-    
-    await init_db()
-    
-    dp.include_routers(start.router)
+
+
+    # ثبت هندلر دستور /start
+    dp.message.register(start_handler, Command(commands=["start"]))
+
+    print("بات در حال اجراست...")
+    await bot.delete_webhook()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
