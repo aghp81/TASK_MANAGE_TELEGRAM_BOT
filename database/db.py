89ca71f7a9349874f412fb11aa1655a65dcd951e
aghp81 <aghp81@gmail.com>
2025-05-23 11:13:29 +0330

    فایل راه‌اندازی دیتابیس

---
+++ database/db.py
@@ -0,0 +1,11 @@
+# db.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+engine = create_engine("sqlite:///bot_database.db", echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
